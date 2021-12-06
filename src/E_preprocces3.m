6e5667633ae1a151dfe1ae6f2c2c16b42f8513b8
douwehorsthuis <douwehorsthuis@gmail.com>
2021-12-06 12:29:28 -0500

    fixing PCA to also look at bridged channels excluding extra cleaning

--- src/E_preprocces3.m
+++ src/E_preprocces3.m
@@ -1,3 +1,12 @@
+% Restingstate pipepline (2021)
+% Final version of SRC code 12/6/2021
+% this script allows for re-reference (which we skip)
+% does an average reference
+% does ICA (using pop_runica) setting a PCA, of all chans minus 1. This is
+% not great. So it's changed to pca = rank(EEG.data), which also takes care of bridged channels)
+% does IC lable (to organize the ICA components and prints them per participant
+% used to do an extra cleaning that is not needed anymore and excluded (12/6/2021)
+
 clear variables
 eeglab
 Group = { 'Control'}; %
@@ -58,85 +67,86 @@ for g=1:length(Group)
         EEG = eeg_checkset( EEG );
         EEG = pop_saveset( EEG, 'filename',[subject_list{s} '_ref.set'],'filepath', data_path);
         
-        %         %because the ICA data + overall data is short seem noisy we clean extra for this group
-        %         if strcmp(Group{g}, 'Aging')
-        %             orig_length=EEG.xmax;
-        %             EEG = pop_rejcont(EEG, 'elecrange',[1:EEG.nbchan] ,'freqlimit',[20 40] ,'threshold',8 ,'epochlength',0.5,'contiguous',4,'addlength',0.25,'taper','hamming');
-        %             clean_length=EEG.xmax;
-        %             deleted_data_before(s,:)=[subject_list(s), 100-(clean_length/orig_length)*100];
-        %         end
-        %         %Independent Component Analysis
-        %         EEG = eeg_checkset( EEG );
-        %         pca = EEG.nbchan-1; %the PCA part of the ICA needs stops the rank-deficiency
-        %         EEG = pop_runica(EEG, 'extended',1,'interupt','on','pca',pca); %using runica function, with the PCA part
-        %         EEG = eeg_checkset( EEG );
-        %         EEG = pop_saveset( EEG, 'filename',[subject_list{s} '_ica.set'],'filepath', data_path);
-        %         %organizing components
-        %         clear bad_components brain_ic muscle_ic eye_ic hearth_ic line_noise_ic channel_ic other_ic
-        %         EEG = iclabel(EEG); %does ICLable function
-        %         ICA_components = EEG.etc.ic_classification.ICLabel.classifications ; %creates a new matrix with ICA components
-        %         %Only the eyecomponent will be deleted, thus only components 3 will be put into the 8 component
-        %         ICA_components(:,8) = ICA_components(:,3); %row 1 = Brain row 2 = muscle row 3= eye row 4 = Heart Row 5 = Line Noise row 6 = channel noise row 7 = other, combining this makes sure that the component also gets deleted if its a combination of all.
-        %         bad_components = find(ICA_components(:,8)>0.80 & ICA_components(:,1)<0.10); %if the new row is over 80% of the component and the component has less the 5% brain
-        %         %Still labeling all the other components so they get saved in the end
-        %         brain_ic = length(find(ICA_components(:,1)>0.80));
-        %         muscle_ic = length(find(ICA_components(:,2)>0.80 & ICA_components(:,1)<0.05));
-        %         eye_ic = length(find(ICA_components(:,3)>0.80 & ICA_components(:,1)<0.05));
-        %         hearth_ic = length(find(ICA_components(:,4)>0.80 & ICA_components(:,1)<0.05));
-        %         line_noise_ic = length(find(ICA_components(:,5)>0.80 & ICA_components(:,1)<0.05));
-        %         channel_ic = length(find(ICA_components(:,6)>0.80 & ICA_components(:,1)<0.05));
-        %         other_ic = length(find(ICA_components(:,7)>0.80 & ICA_components(:,1)<0.05));
-        %         %Plotting all eye componentes and all remaining components
-        %         if isempty(bad_components)~= 1 %script would stop if people lack bad components
-        %             if ceil(sqrt(length(bad_components))) == 1
-        %                 pop_topoplot(EEG, 0, [bad_components bad_components] ,subject_list{s} ,0,'electrodes','on');
-        %             else
-        %                 pop_topoplot(EEG, 0, [bad_components] ,subject_list{s},[ceil(sqrt(length(bad_components))) ceil(sqrt(length(bad_components)))] ,0,'electrodes','on');
-        %             end
-        %             title(subject_list{s});
-        %             print([figure_path subject_list{s} '_Bad_ICs_topos'], '-dpng' ,'-r300');
-        %             EEG = pop_subcomp( EEG, [bad_components], 0); %excluding the bad components
-        %             close all
-        %         else %instead of only plotting bad components it will plot all components
-        %             title(subject_list{s}); text( 0.2,0.5, 'there are no eye-components found')
-        %             print([figure_path subject_list{s} '_Bad_ICs_topos'], '-dpng' ,'-r300');
-        %         end
-        %         title(subject_list{s});
-        %         pop_topoplot(EEG, 0, 1:size(EEG.icaweights,1) ,subject_list{s},[ceil(sqrt(size(EEG.icaweights,1))) ceil(sqrt(size(EEG.icaweights,1)))] ,0,'electrodes','on');
-        %         print([figure_path subject_list{s} '_remaining_ICs_topos'], '-dpng' ,'-r300');
-        %         close all
-        %         %putting both figures in 1 plot saving it, deleting the other 2.
-        %         figure('units','normalized','outerposition',[0 0 1 1])
-        %         if EEG.nbchan<65
-        %             subplot(1,5,1);
-        %         else
-        %             subplot(1,10,1);
-        %         end
-        %         imshow([figure_path subject_list{s} '_Bad_ICs_topos.png']);
-        %         title('Deleted components')
-        %         if EEG.nbchan<65
-        %             subplot(1,5,2:5);
-        %         else
-        %             subplot(1,10,2:10);
-        %         end
-        %         imshow([figure_path subject_list{s} '_remaining_ICs_topos.png']);
-        %         title('Remaining components')
-        %         print([figure_path subject_list{s} '_ICs_topos'], '-dpng' ,'-r300');
-        %         %deleting two original files
-        %         delete([figure_path subject_list{s} '_Bad_ICs_topos.png'])
-        %         delete([figure_path subject_list{s} '_remaining_ICs_topos.png'])
-        %         close all
-        %         EEG = pop_saveset( EEG, 'filename',[subject_list{s} '_excom.set'],'filepath', data_path);%save
-        %         %% extra cleaning
-        %         orig_length=EEG.xmax;
-        %         EEG = pop_rejcont(EEG, 'elecrange',[1:EEG.nbchan] ,'freqlimit',[20 40] ,'threshold',8 ,'epochlength',0.5,'contiguous',4,'addlength',0.25,'taper','hamming');
-        %         clean_length=EEG.xmax;
-        %         EEG = pop_saveset( EEG, 'filename',[subject_list{s} '_clean.set'],'filepath', data_path);%save
-        %         %% saving structures
-        %         deleted_data(s,:)=[subject_list(s), 100-(clean_length/orig_length)*100];
-        %         subj_comps=[subject_list(s), num2cell(brain_ic), num2cell(muscle_ic), num2cell(eye_ic), num2cell(hearth_ic), num2cell(line_noise_ic), num2cell(channel_ic), num2cell(other_ic)];
-        %         components(s,:)=[subj_comps];
+        %because the ICA data + overall data is short seem noisy we clean extra for this group
+        if strcmp(Group{g}, 'Aging')
+            orig_length=EEG.xmax;
+            EEG = pop_rejcont(EEG, 'elecrange',[1:EEG.nbchan] ,'freqlimit',[20 40] ,'threshold',8 ,'epochlength',0.5,'contiguous',4,'addlength',0.25,'taper','hamming');
+            clean_length=EEG.xmax;
+            deleted_data_before(s,:)=[subject_list(s), 100-(clean_length/orig_length)*100];
+        end
+        %Independent Component Analysis
+        EEG = eeg_checkset( EEG );
+        %pca = EEG.nbchan-1; %the PCA part of the ICA needs stops the rank-deficiency % pre 12/6/2021
+        pca = rank(EEG.data); %this takes care of avg ref and bridge channels
+        EEG = pop_runica(EEG, 'extended',1,'interupt','on','pca',pca); %using runica function, with the PCA part
+        EEG = eeg_checkset( EEG );
+        EEG = pop_saveset( EEG, 'filename',[subject_list{s} '_ica.set'],'filepath', data_path);
+        %organizing components
+        clear bad_components brain_ic muscle_ic eye_ic hearth_ic line_noise_ic channel_ic other_ic
+        EEG = iclabel(EEG); %does ICLable function
+        ICA_components = EEG.etc.ic_classification.ICLabel.classifications ; %creates a new matrix with ICA components
+        %Only the eyecomponent will be deleted, thus only components 3 will be put into the 8 component
+        ICA_components(:,8) = ICA_components(:,3); %row 1 = Brain row 2 = muscle row 3= eye row 4 = Heart Row 5 = Line Noise row 6 = channel noise row 7 = other, combining this makes sure that the component also gets deleted if its a combination of all.
+        bad_components = find(ICA_components(:,8)>0.80 & ICA_components(:,1)<0.10); %if the new row is over 80% of the component and the component has less the 5% brain
+        %Still labeling all the other components so they get saved in the end
+        brain_ic = length(find(ICA_components(:,1)>0.80));
+        muscle_ic = length(find(ICA_components(:,2)>0.80 & ICA_components(:,1)<0.05));
+        eye_ic = length(find(ICA_components(:,3)>0.80 & ICA_components(:,1)<0.05));
+        hearth_ic = length(find(ICA_components(:,4)>0.80 & ICA_components(:,1)<0.05));
+        line_noise_ic = length(find(ICA_components(:,5)>0.80 & ICA_components(:,1)<0.05));
+        channel_ic = length(find(ICA_components(:,6)>0.80 & ICA_components(:,1)<0.05));
+        other_ic = length(find(ICA_components(:,7)>0.80 & ICA_components(:,1)<0.05));
+        %Plotting all eye componentes and all remaining components
+        if isempty(bad_components)~= 1 %script would stop if people lack bad components
+            if ceil(sqrt(length(bad_components))) == 1
+                pop_topoplot(EEG, 0, [bad_components bad_components] ,subject_list{s} ,0,'electrodes','on');
+            else
+                pop_topoplot(EEG, 0, [bad_components] ,subject_list{s},[ceil(sqrt(length(bad_components))) ceil(sqrt(length(bad_components)))] ,0,'electrodes','on');
+            end
+            title(subject_list{s});
+            print([figure_path subject_list{s} '_Bad_ICs_topos'], '-dpng' ,'-r300');
+            EEG = pop_subcomp( EEG, [bad_components], 0); %excluding the bad components
+            close all
+        else %instead of only plotting bad components it will plot all components
+            title(subject_list{s}); text( 0.2,0.5, 'there are no eye-components found')
+            print([figure_path subject_list{s} '_Bad_ICs_topos'], '-dpng' ,'-r300');
+        end
+        title(subject_list{s});
+        pop_topoplot(EEG, 0, 1:size(EEG.icaweights,1) ,subject_list{s},[ceil(sqrt(size(EEG.icaweights,1))) ceil(sqrt(size(EEG.icaweights,1)))] ,0,'electrodes','on');
+        print([figure_path subject_list{s} '_remaining_ICs_topos'], '-dpng' ,'-r300');
+        close all
+        %putting both figures in 1 plot saving it, deleting the other 2.
+        figure('units','normalized','outerposition',[0 0 1 1])
+        if EEG.nbchan<65
+            subplot(1,5,1);
+        else
+            subplot(1,10,1);
+        end
+        imshow([figure_path subject_list{s} '_Bad_ICs_topos.png']);
+        title('Deleted components')
+        if EEG.nbchan<65
+            subplot(1,5,2:5);
+        else
+            subplot(1,10,2:10);
+        end
+        imshow([figure_path subject_list{s} '_remaining_ICs_topos.png']);
+        title('Remaining components')
+        print([figure_path subject_list{s} '_ICs_topos'], '-dpng' ,'-r300');
+        %deleting two original files
+        delete([figure_path subject_list{s} '_Bad_ICs_topos.png'])
+        delete([figure_path subject_list{s} '_remaining_ICs_topos.png'])
+        close all
+        EEG = pop_saveset( EEG, 'filename',[subject_list{s} '_excom.set'],'filepath', data_path);%save
+        %% extra cleaning %was uses before 12/6/2021 for all projects, not needed after update to cleaning function
+        %orig_length=EEG.xmax;
+        %EEG = pop_rejcont(EEG, 'elecrange',[1:EEG.nbchan] ,'freqlimit',[20 40] ,'threshold',8 ,'epochlength',0.5,'contiguous',4,'addlength',0.25,'taper','hamming');
+        %clean_length=EEG.xmax;
+        %EEG = pop_saveset( EEG, 'filename',[subject_list{s} '_clean.set'],'filepath', data_path);%save
+        %% saving structures
+        deleted_data(s,:)=[subject_list(s), 100-(clean_length/orig_length)*100];
+        subj_comps=[subject_list(s), num2cell(brain_ic), num2cell(muscle_ic), num2cell(eye_ic), num2cell(hearth_ic), num2cell(line_noise_ic), num2cell(channel_ic), num2cell(other_ic)];
+        components(s,:)=[subj_comps];
     end
-    %     save([home_path 'components'], 'components');
-    %     save([home_path 'deleted_data'], 'deleted_data');
+    save([home_path 'components'], 'components');
+    save([home_path 'deleted_data'], 'deleted_data');
 end
