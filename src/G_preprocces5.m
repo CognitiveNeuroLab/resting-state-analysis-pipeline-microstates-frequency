e2b3690f2af18da7298950adae8e99b227c12705
douwehorsthuis <douwehorsthuis@gmail.com>
2021-12-07 12:56:04 -0500

    final version

--- src/G_preprocces5.m
+++ src/G_preprocces5.m
@@ -1,9 +1,11 @@
 % Restingstate pipepline (2021)
 % Final version of SRC code 12/6/2021
-% because in the pre 12/6/2021, we decided to delete bad parts of the
+% creates separate Eyes open and Eyes closed files
+% because in the pre 12/6/2021 scripts, we decided to delete bad parts of the
 % continues data after the D_preproccess2 script (where it adds missing triggers)
-% we needed to run this script.
-% The new scripts do this cleaning as a part of deleting channels and thus we DO NOT need to run it
+% we needed to re-add triggers.
+% The new scripts do this cleaning as a part of deleting channels and thus
+% we should not see any missing triggers. But it cannot harm to double check 
 clear variables
 eeglab
 
