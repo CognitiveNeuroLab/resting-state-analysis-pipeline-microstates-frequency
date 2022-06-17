62a3dcbc72b82b9b358dbf4eee41f3f8a127c1f3
douwehorsthuis <douwehorsthuis@gmail.com>
2022-06-17 15:25:39 -0400

    final version matlab code

--- src/I_Microstates_groups.m
+++ src/I_Microstates_groups.m
@@ -5,13 +5,14 @@
 % so that you can decide how many microstates
 % you want to choose without getting into single subjects
 % only run 1 group at a time to compaire their EO and EC data
-% adapted for our pipeline on 12/20/2021 by Douwe
+% or run for everyone together 
+% adapted for our pipeline on 6/26/2021 by Douwe
 clear variables
 
 
 
 
-Group = {'22q' };% 'Control 22q' 'Control sz' '22q +' '22q -' 'sz' };%
+Group = {'All' };% 'Control 22q' 'Control sz' '22q +' '22q -' 'sz' };%
 type={'EC'};
 for g=1:length(Group)
     switch Group{g}
@@ -35,6 +36,9 @@ for g=1:length(Group)
             subject_list = {'2202' '2204' '2212' '2222' '2229' '2231' '2257' '2260' '2261' '2284' '2295'};
         case  '22q'
             subject_list = {'2201','2207','2216','2243','2256','2267','2274','2281','2286','2292','2202','2204','2212','2222','2229','2231','2257','2260','2261','2284','2295'};
+        case  'All'
+            subject_list = {'10293' '10561' '10562' '10581' '10616' '10748' '10822' '10858' '10935' '12004' '12512' '12632' '12648' '12727' '12739' '12746' '12750'  '12770' '12815' '12010' '12139' '12177' '12188' '12197' '12203' '12206'  '12272'  '12415' '12482' '12588' '12651' '12852' '12870' '7003' '7007' '7019' '7025' '7046' '7051' '7054' '7058'  '7061' '7064' '7065' '7073'  '7078' '7089' '7092' '7094' '7123' '7556' '7808' '2201' '2207' '2216' '2243' '2256' '2267' '2274' '2281' '2286' '2292' '2202' '2204' '2212' '2222' '2229' '2231' '2257' '2260','2261' '2284' '2295'};
+            
     end
     home_path = 'D:\restingstate\data\';
     for t=1:length(type)
@@ -43,7 +47,7 @@ for g=1:length(Group)
         for s=1:length(subject_list)
             data_path  = [home_path subject_list{s} ''];% Path to the folder containing the current subject's data
             % Load original dataset
-            fprintf('\n\n\n**** %s: Loading dataset ****\n\n\n', subject_list{s});
+            fprintf('\n\n\n**** %s: Loading dataset ****\n\n\n',  subject_list{s});
             EEG = pop_loadset('filename', [subject_list{s} '_' type{t} '.set'], 'filepath', data_path);
             [ALLEEG, EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
         end
@@ -57,7 +61,7 @@ for g=1:length(Group)
         figure;[tt]=title([type{t} ' ' Group{g}]);tt.FontSize = 15;MicroPlotTopo( EEG, 'plot_range', [] ); %plotting microstates
         print([save_path Group{g} '_microstate_' type{t}], '-djpeg','-r300');
         figure();
-        % EEG = pop_micro_selectNmicro( EEG ); % only select CV and GEV, look for where GEV doesn't increase significantly
+        EEG = pop_micro_selectNmicro( EEG ); % only select CV and GEV, look for where GEV doesn't increase significantly
         [ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
         close all
     end
